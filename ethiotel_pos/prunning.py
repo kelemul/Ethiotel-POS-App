a422e46873f4fa71fe4c9bc3bbe315c1d05753ad
kelemul <kelemu3509@gmail.com>
2026-09-01 09:06:47 +0300

    some workspace working

--- ethiotel_pos/prunning.py
+++ ethiotel_pos/prunning.py
@@ -18,7 +18,8 @@ MANIFEST_FILE = "lite_mode_lock_manifest.json"
 def get_lit_modules():
     """Safely fetch Allowed Modules dynamically to avoid import-time DB errors."""
     lit_modules = [
-        "Selling", "Ethio Telecom POS App", "Ethiotel_POS","Sales and Marketing","Buying"
+        "Selling", "Ethio Telecom POS App", 
+        "Ethiotel_POS","Sales and Marketing","Buying","Core","Accounts","Stock","Setup"
     ]
     try:
         # Include all core frappe modules by default
